11e6dce3d52202972d5aa51ec47d15cde971f073
Ashley Mathew <amathew195@gmail.com>
2022-11-07 13:48:45 -0600

    updating model methods on users

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const bcrypt = require('bcrypt')
+
 /** User of the site. */
 
 class User {
@@ -9,22 +11,64 @@ class User {
    */
 
   static async register({ username, password, first_name, last_name, phone }) {
+
+      const result = await db.query(
+        `INSERT INTO users (username,
+          password,
+          first_name,
+          last_name,
+          phone)
+          VALUES
+          ($1, $2, $3, $4, $5)
+          RETURNING username, password, first_name, last_name, phone`,
+          [username, password, first_name, last_name, phone]
+      );
+
+
+    return result.rows[0];
   }
 
   /** Authenticate: is username/password valid? Returns boolean. */
 
   static async authenticate(username, password) {
+    const result = await db.query(
+      `SELECT password
+      FROM users
+      WHERE username = $1`,
+      [username]
+    );
+    const user = result.rows[0];
+
+    if (user){
+      return (await bcrypt.compare(password, user.password) === true);
+    }
   }
 
   /** Update last_login_at for user */
 
   static async updateLoginTimestamp(username) {
+    const result = await db.query(
+      `UPDATE users
+      SET last_login_at = current_timestamp
+      WHERE username = $1
+      RETURNING username, last_login_at`,
+      [username]
+    );
+    const user = result.rows[0];
+
+    if (!user) throw new NotFoundError(`No such username: ${username}`);
   }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name}, ...] */
 
   static async all() {
+    const results = await db.query(
+      `SELECT username, first_name, last_name
+      FROM users`
+    )
+    const users = results.rows;
+    return users;
   }
 
   /** Get: get user by username
